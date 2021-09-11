e775fe47aede7b5050170e6f539b5a6a6a88a820
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-09-11 12:12:14 +0530

    Add project files.

---
+++ AddressBook_LINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Using LINQ !");
+            Console.WriteLine("------------------------------------");
+        }
+    }
+}
